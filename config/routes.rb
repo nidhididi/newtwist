df5ed15b81723c281b863cf4deae607e43520f7e
Nidhi Mehta <nidhididi@gmail.com>
2014-03-06 16:41:33 -0500

    thursday fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ NewTwist::Application.routes.draw do
   get "welcome/index"
   get "welcome/about"
   get "welcome/contact"
+  
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
