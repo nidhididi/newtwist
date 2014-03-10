a8c3f2f1d797e3253cfeb6357591cbecabfcb381
Nidhi Mehta <nidhididi@gmail.com>
2014-03-10 12:07:54 -0400

    aligned pic in navbar

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "save integer to birth year" do
+  	test_user = User.new
+  	test_user.email = "testtest@mail.com"
+
+  	rand (1000...9999)
+  	if test_user.save
+  		assert true
+  	else
+  		assert false
+  end
+end
 end
